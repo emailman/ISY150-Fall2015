feef18c2a8a1a8b52234c96d0cb5430a8fc21ab2
emailman <emailman@dtcc.edu>
2015-10-22 09:42:33 -0400

    Removed unneeded parens

--- p1/oct22a.py
+++ p1/oct22a.py
@@ -1,6 +1,6 @@
 __author__ = 'emailman'
 
 reply = -1
-while (reply != 0):
+while reply != 0:
     reply = int(input("Enter a positive integer, zero when done: "))
     print("Your input doubled is", reply*2)
