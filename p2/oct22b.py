52332aab67d5ebff770af1ab09ea18e007625051
emailman <emailman@dtcc.edu>
2015-10-23 13:57:09 -0400

    Fixed logic in oct22b.py

--- p2/oct22b.py
+++ p2/oct22b.py
@@ -3,12 +3,13 @@ __author__ = 'emailman'
 done = False
 while not done:
     try:
-        reply1, reply2 = input('Enter two integers, zero when done: ').split()
+        reply1, reply2 = input('Enter two integers, two zeros when done: ').split()
         int1, int2 = [int(reply1), int(reply2)]
-        if int1 != 0:
-            print("The sum of your inputs = ", int1 + int2)
-        else:
+        if int1 == 0 and int2 ==0:
             done = True
+        else:
+            print("The sum of your inputs =", int1 + int2)
     except:
-        print("You did not enter a integer")
+        print("You did not enter two integers")
+
 print("All done")
