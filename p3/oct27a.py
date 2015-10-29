353ccbae52e8873f31243878cfe4771dd5582cdc
emailman <emailman@dtcc.edu>
2015-10-29 10:27:07 -0400

    Removed unneeded parens

---
+++ p3/oct27a.py
@@ -0,0 +1,22 @@
+__author__ = 'emailman'
+
+for i in [33.5, 0, -2, 'eric']:
+    print(i)
+
+even_numbers = [2, 4, 6, 8]
+print(even_numbers)
+print(len(even_numbers))
+
+odd_numbers = list(range(1, 100, 2))
+print(odd_numbers)
+# print(odd_numbers[3])
+
+for i in range(3):
+    print(odd_numbers[i])
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+for i in range(len(my_tuple)):
+    print(my_tuple[i])
+
---
+++ p4/oct29a.py
@@ -0,0 +1,10 @@
+__author__ = 'emailman'
+
+def add(arg1, arg2):
+    return arg1 + arg2
+
+num1 = 3
+num2 = 4
+print('Twice the sum is', add(arg1=num1, arg2=num2) * 2)
+
+
