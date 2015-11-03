9eb334c69a6c5286e9c7b60b7ae7337183140e54
emailman <emailman@dtcc.edu>
2015-11-03 08:51:02 -0500

    corrected logical error

--- p6/oct29d.py
+++ p6/oct29d.py
@@ -4,11 +4,11 @@ __author__ = 'emailman'
 # Note this function uses a conditional expression
 # and returns a list
 def min_max(arg1, arg2):
-    return [arg1, arg2] if arg1 < arg2 else [arg1, arg2]
+    return [arg1, arg2] if arg1 < arg2 else [arg2, arg1]
 
-num1 = 12
+num1 = 1
 num2 = 5
 
 # Note two values are passed to the function and two values are returned
 smaller, larger = min_max(num1, num2)
-print('smaller=', smaller, "larger=", larger)
+print('smaller=', smaller, 'larger=', larger)
