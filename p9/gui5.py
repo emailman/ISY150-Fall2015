b443c691fe6556f4c2fbb88b1d92a8dd070b2944
emailman <emailman@dtcc.edu>
2015-11-08 08:35:08 -0500

    Cleanup gui5

--- p9/gui5.py
+++ p9/gui5.py
@@ -16,35 +16,45 @@ def main():
     window.title("Loan Calculator")
 
     # Use the grid manager to create labels in column 0
-    Label(window, text="Annual Interest Rate").grid(row=1, column=0, sticky=W)
-    Label(window, text="Number of Years").grid(row=2, column=0, sticky=W)
-    Label(window, text="Loan Payment").grid(row=3, column=0, sticky=W)
-    Label(window, text="Monthly Payment", fg="BLUE").grid(row=4, column=0, sticky=W)
-    Label(window, text="Total Payment", fg="BLUE").grid(row=5, column=0, sticky=W)
+    Label(window, text="Annual Interest Rate").\
+        grid(row=1, column=0, sticky=W, padx="5", pady="2")
+    Label(window, text="Number of Years").\
+        grid(row=2, column=0, sticky=W, padx="5", pady="2")
+    Label(window, text="Loan Payment").\
+        grid(row=3, column=0, sticky=W, padx="5", pady="2")
+    Label(window, text="Monthly Payment", fg="BLUE")\
+        .grid(row=4, column=0, sticky=W, padx="5", pady="5")
+    Label(window, text="Total Payment", fg="BLUE")\
+        .grid(row=5, column=0, sticky=W, padx="5", pady="2")
 
     # Create text fields in column 1
     global annual_interest_rate
     annual_interest_rate = StringVar()
-    Entry(window, textvariable=annual_interest_rate, justify=RIGHT).grid(row=1, column=1)
+    Entry(window, textvariable=annual_interest_rate, justify=RIGHT).\
+        grid(row=1, column=1, padx="5", pady="2")
 
     global number_of_years
     number_of_years = StringVar()
-    Entry(window, textvariable=number_of_years, justify=RIGHT).grid(row=2, column=1)
+    Entry(window, textvariable=number_of_years, justify=RIGHT).\
+        grid(row=2, column=1, padx="5", pady="2")
 
     global loan_amount
     loan_amount = StringVar()
-    Entry(window, textvariable=loan_amount, justify=RIGHT).grid(row=3, column=1)
+    Entry(window, textvariable=loan_amount, justify=RIGHT).\
+        grid(row=3, column=1, padx="5", pady="2")
 
     global monthly_payment
     monthly_payment = StringVar()
-    Entry(window, textvariable=monthly_payment, justify=RIGHT, state="disabled").grid(row=4, column=1)
+    Entry(window, textvariable=monthly_payment, justify=RIGHT, state="disabled").\
+        grid(row=4, column=1, padx="5", pady="5")
 
     global total_payment
     total_payment = StringVar()
-    Entry(window, textvariable=total_payment, justify=RIGHT, state="disabled").grid(row=5, column=1)
+    Entry(window, textvariable=total_payment, justify=RIGHT, state="disabled").\
+        grid(row=5, column=1, padx="5", pady="2")
 
     # Create two buttons in row 6
-    Button(window, bg="RED", text="Quit",
+    Button(window, bg="RED", fg="BLUE", text="Quit",
            command=window.destroy).grid(row=6, column=0, padx=20, pady=10, sticky=W)
 
     Button(window, bg="GREEN", fg="YELLOW", text="Calculate", command=payment_calc)\
